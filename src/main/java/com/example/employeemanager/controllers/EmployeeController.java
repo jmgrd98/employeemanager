79fc52a516e3d82130fd491baf1c3a494d0974d3
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-14 08:38:15 -0300

    Create EmployeeController.java

---
+++ src/main/java/com/example/employeemanager/controllers/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.example.employeemanager.controllers;
+
+import com.example.employeemanager.models.Employee;
+import com.example.employeemanager.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<List<Employee>>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
+        Employee employee = employeeService.findEmployeeById(id);
+        return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+    }
+
+}
